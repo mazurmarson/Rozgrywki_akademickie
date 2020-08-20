40cee40c971232a5a9739b151040c8a1489f631f
Marek Mazur <mazi270tv@gmail.com>
2020-08-20 19:13:26 +0200

    Dodanie dokumentacji, kosmetyczne zmiany, dodanie tla

--- RozgrywkiAkademickie4/Models/Sport.cs
+++ RozgrywkiAkademickie4/Models/Sport.cs
@@ -11,7 +11,7 @@ namespace RozgrywkiAkademickie4.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "Nazwa sportu jest wymagana")]
         [StringLength(100, ErrorMessage = "Nazwa sportu jest za długa")]
-        [RegularExpression(@"^[A-Z][a-z]([a-z]| ?)*",
+        [RegularExpression(@"^[A-ZzżźćńółęąśŻŹĆĄŚĘŁÓŃ][a-zzżźćńółęąśŻŹĆĄŚĘŁÓŃ]([a-zzżźćńółęąśŻŹĆĄŚĘŁÓŃ]| ?)*",
          ErrorMessage = "Nazwa sportu musi zaczynać sie z wielkiej litery i nie może zawierać liczb.")]
         public string Nazwa { get; set; }
     }
